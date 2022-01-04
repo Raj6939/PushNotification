e15c2021dd2300325519507bee0e03252096187a
rajpatil6939 <raj.v.patil108@gmail.com>
2022-01-04 16:05:22 +0530

    firebase push notification

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,31 @@
+importScripts("https://www.gstatic.com/firebasejs/8.2.4/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.4/firebase-messaging.js");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjGhW6K1J6hrWdbBV_CpmrWj_Mmvk_6GM",
+  authDomain: "my-web-push-1dc51.firebaseapp.com",
+  databaseURL: "https://my-web-push-1dc51-default-rtdb.firebaseio.com",
+  projectId: "my-web-push-1dc51",
+  storageBucket: "my-web-push-1dc51.appspot.com",
+  messagingSenderId: "412483144779",
+  appId: "1:412483144779:web:2ec1e879e2ab666c7751ce",
+  measurementId: "G-PFPSSDG7G1"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+self.addEventListener('push', function (event) {
+    var data = event.data.json();
+    
+    const title = data.Title;
+    data.Data.actions = data.Actions;
+    const options = {
+      body: data.Message,
+      data: data.Data
+    };
+    event.waitUntil(self.registration.showNotification(title, options));
+  });
+    
+  self.addEventListener('notificationclick', function (event) {});
+    
+  self.addEventListener('notificationclose', function (event) {});
